d88e8ec5dccc31965c3c1b7fd79c8dbed80365f4
Julian Yepes <julianyepes2593@gmail.com>
2020-09-30 16:06:07 -0500

    implementacion de un bloque, pero ya en la funcion

--- src/helpers/read.js
+++ src/helpers/read.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 
 function leer(directorio) {
     let rutas = []
-    fs.readdirSync(directorio).forEach(file => {
+    fs.readdirSync(directorio)
+    for (let file of directorio) {
         file = directorio + file
         rutas.push(file)
-    });
+    }
     return rutas
 }
 
