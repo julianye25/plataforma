6da19e3e3486fc16ca1ba23bd594d0479060b4d1
Julian Yepes <julianyepes2593@gmail.com>
2020-10-01 10:27:34 -0500

    edicion en configuracion

--- src/server/config.js
+++ src/server/config.js
@@ -15,7 +15,12 @@ module.exports = app => {
         res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         res.header('Access-Control-Allow-Methods', "POST, GET, PUT, DELETE, OPTIONS");
-        next();
+        if (req.method === 'OPTIONS') {
+            res.end();
+        } else {
+
+            next();
+        }
     });
 
     // settings
