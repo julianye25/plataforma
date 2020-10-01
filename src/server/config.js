8f961026ddf8a127ebc9c68e640e31c7be0313a1
Julian Yepes <julianyepes2593@gmail.com>
2020-10-01 15:58:43 -0500

    probando funcionalidedes en el cors

--- src/server/config.js
+++ src/server/config.js
@@ -18,6 +18,7 @@ module.exports = app => {
         res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
         res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+        req.setTimeout(0);
         next();
     });
 
