0f58a30038488a91553ff68fa2d8d16fea35f91a
Julian Yepes <julianyepes2593@gmail.com>
2020-10-01 15:03:31 -0500

    probando funcionalidedes en el cors

--- src/server/config.js
+++ src/server/config.js
@@ -14,7 +14,7 @@ const { TIMEOUT } = require('dns');
 module.exports = app => {
     // CORS
     app.use(function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:4200', always);
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
         res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
